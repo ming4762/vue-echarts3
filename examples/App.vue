b6bb2b259b9898f93a0a8bdaa8a07a0ea3710675
shizhongming <zhongming4762@hotmail.com>
2021-04-05 20:25:03 +0800

    完善echarts事件

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="height: 500px; width: 800px; background: white">
-    <vue-echarts3 :options="{}" ref="vueEcharts"/>
+    <vue-echarts3 :dblclick="handleClick" :options="options" ref="vueEcharts"/>
+    <button @click="handleTest">测试</button>
   </div>
 </template>
 
@@ -9,6 +10,34 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
+  data () {
+    return {
+      options: {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      }
+    }
+  },
+  methods: {
+    handleTest () {
+      this.options.title.text = 'ECharts 入门示例2'
+      this.options.series[0].data[0] = 10
+    },
+    handleClick (params: any) {
+      console.log(params)
+    }
+  },
   mounted () {
     console.log(this.$refs.vueEcharts)
   }
--- packages/echarts/src/Echarts.tsx
+++ packages/echarts/src/Echarts.tsx
@@ -1,5 +1,8 @@
 import { defineComponent, PropType, ref, computed, toRefs } from 'vue'
 
+import { addListener, removeListener } from 'resize-detector'
+import { debounce } from 'lodash'
+
 import * as echarts from 'echarts'
 
 import { InitOption } from '../../../typings'
@@ -21,6 +24,48 @@ const createUuid = (): string => {
 }
 
 /**
+ * echarts事件
+ */
+const EVENTS = [
+  'dblclick',
+  'legendselectchanged',
+  'legendselected',
+  'legendunselected',
+  'legendscroll',
+  'datazoom',
+  'datarangeselected',
+  'timelinechanged',
+  'timelineplaychanged',
+  'restore',
+  'dataviewchanged',
+  'magictypechanged',
+  'geoselectchanged',
+  'geoselected',
+  'geounselected',
+  'pieselectchanged',
+  'pieselected',
+  'pieunselected',
+  'mapselectchanged',
+  'mapselected',
+  'mapunselected',
+  'axisareaselected',
+  'focusnodeadjacency',
+  'unfocusnodeadjacency',
+  'brush',
+  'brushselected',
+  'rendered',
+  'finished',
+  'click',
+  'mouseover',
+  'mouseout',
+  'mousemove',
+  'mousedown',
+  'mouseup',
+  'globalout',
+  'contextmenu'
+]
+
+/**
  * 判断charts是否有数据
  */
 const hasData = (manualOptions: any, options: any): boolean => {
@@ -40,30 +85,6 @@ const hasData = (manualOptions: any, options: any): boolean => {
     }
   }
   return result
-}
-
-/**
- * 创建表格
- */
-const createChart = (id: string): echarts.ECharts => {
-  // 创建表格队形
-  const chart = echarts.init(document.getElementById(id) as HTMLElement)
-  chart.setOption({
-    title: {
-      text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    xAxis: {
-      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    },
-    yAxis: {},
-    series: [{
-      name: '销量',
-      type: 'bar',
-      data: [5, 20, 36, 10, 10, 20]
-    }]
-  })
-  return chart
 }
 
 /**
@@ -89,14 +110,33 @@ export default defineComponent({
       default: createUuid()
     },
     initOptions: {
-      type: Object as PropType<InitOption>,
-      required: true
+      type: Object as PropType<InitOption>
     },
+    // 当前 ECharts 实例使用的主题
     theme: {
-      type: [String, Object]
+      type: [String, Object] as PropType<string>
     },
     options: {
       type: Object
+    },
+    // 实例的分组，会自动绑定到 ECharts 组件的同名属性上
+    group: {
+      type: String as PropType<string>
+    },
+    // 是否浅监控属性变化
+    watchShallow: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    // 在性能敏感（数据量很大）的场景下，我们最好对于 options prop 绕过 Vue 的响应式系统。当将 manual-update prop 指定为 true 且不传入 options prop 时，数据将不会被监听。然后，你需要用 ref 获取组件实例以后手动调用 mergeOptions 方法来更新图表
+    manualUpdate: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    // 指定 ECharts 实例在组件根元素尺寸变化时是否需要自动进行重绘
+    autoresize: {
+      type: Boolean as PropType<boolean>,
+      default: false
     }
   },
   setup (props) {
@@ -104,7 +144,8 @@ export default defineComponent({
     const manualOptions = ref(null)
     // 是否有数据计算属性
     const getHasData = computed(() => hasData(manualOptions.value, options ? options.value : null))
-    const computedDivStyle = computed(() => getDivStyle(initOptions ? initOptions.value : null))
+    // 计算表格高度
+    const computedDivStyle = computed(() => getDivStyle(initOptions && initOptions.value ? initOptions.value : null))
     return {
       manualOptions,
       getHasData,
@@ -114,13 +155,65 @@ export default defineComponent({
   data () {
     const chart: any = null
     return {
-      chart
+      chart,
+      lastArea: 0,
+      resizeHandler: null as any,
+      // 没有数据的配置
+      noDataOption: {
+        title: {
+          text: '暂无数据',
+          x: 'center',
+          borderColor: '#ccc',
+          top: '40%',
+          textStyle: {
+            color: '#888',
+            fontStyle: 'normal',
+            fontWeight: 'normal',
+            fontFamily: 'sans-serif',
+            fontSize: 28
+          }
+        }
+      }
     }
+  },
+  created () {
+    if (!this.manualUpdate) {
+      // 延迟加载不监控options变化
+      this.$watch('options', (_new: any, old: any) => {
+        if (!this.chart && _new) {
+          this.init()
+        } else {
+          this.chart.setOption(_new, _new !== old)
+        }
+      }, { deep: !this.watchShallow })
+    }
+    const watched = ['theme', 'initOptions', 'autoresize', 'manualUpdate', 'watchShallow']
+    watched.forEach(prop => {
+      this.$watch(prop, () => {
+        this.refresh()
+      }, { deep: true })
+    })
   },
   mounted () {
     // 初始化表格
     if (this.options) {
-      this.chart = createChart(this.id)
+      this.init()
+    }
+  },
+  /**
+   * 页面激活执行
+   */
+  activated () {
+    if (this.autoresize) {
+      this.chart && this.chart.resize()
+    }
+  },
+  /**
+   * 销毁页面前销毁chart
+   */
+  beforeUnmount () {
+    if (this.chart) {
+      this.destroy()
     }
   },
   methods: {
@@ -129,6 +222,131 @@ export default defineComponent({
      */
     getArea () {
       return this.$el.offsetWidth * this.$el.offsetHeight
+    },
+    /**
+     * 初始化函数
+     */
+    init () {
+      if (!this.chart && this.getHasData) {
+        const chart = echarts.init(this.$el, this.theme, this.initOptions)
+        // 绑定分组
+        if (this.group) {
+          chart.group = this.group
+        }
+        chart.setOption(this.manualOptions || this.options || {}, true)
+        // 绑定事件
+        EVENTS.forEach(event => {
+          const handler = this.$attrs[event]
+          if (handler) {
+            if (typeof handler === 'function') {
+              chart.on(event, params => {
+                handler.call(this, params)
+              })
+            } else {
+              console.warn(`event is not function, event name: ${event}`)
+            }
+          }
+        })
+        // 绑定autoresize
+        if (this.autoresize) {
+          this.lastArea = this.getArea()
+          this.resizeHandler = debounce(() => {
+            if (this.lastArea === 0) {
+              this.mergeOptions({}, true)
+              this.resize()
+              // this.mergeOptions(this.getOption || {}, true)
+              this.mergeOptions(this.options || this.manualOptions || {}, true)
+            } else {
+              this.resize()
+            }
+            this.lastArea = this.getArea()
+          }, 100, { leading: true })
+          addListener(this.$el, this.resizeHandler)
+        }
+        this.chart = chart
+      }
+    },
+    // 刷新echart
+    refresh: function () {
+      if (this.chart) {
+        this.destroy()
+        this.init()
+      }
+    },
+    // 销毁charts
+    destroy: function () {
+      if (this.autoresize) {
+        // 移除事件
+        removeListener(this.$el, this.resizeHandler)
+      }
+      this.dispose()
+      this.chart = null
+    },
+    dispose: function () {
+      this.delegateMethod('dispose')
+    },
+    /**
+     * 执行echart方法
+     */
+    delegateMethod: function (name: string, ...args: Array<any>) {
+      if (!this.chart) {
+        this.init()
+      }
+      return this.chart[name](...args)
+    },
+    // 执行echart方法resize
+    resize: function (options?: any) {
+      this.delegateMethod('resize', options)
+    },
+    // 执行echart方法appendData
+    appendData: function (params: any) {
+      this.delegateMethod('appendData', params)
+    },
+    // 执行echart方法dispatchAction
+    dispatchAction: function (payload: any) {
+      this.delegateMethod('dispatchAction', payload)
+    },
+    // 执行echart方法convertToPixel
+    convertToPixel: function (finder: any, value: any) {
+      this.delegateMethod('convertToPixel', finder, value)
+    },
+    // 执行echart方法convertFromPixel
+    convertFromPixel: function (finder: any, value: any) {
+      this.delegateMethod('convertFromPixel', finder, value)
+    },
+    // 执行echart方法containPixel
+    containPixel: function (finder: any, value: any) {
+      this.delegateMethod('containPixel', finder, value)
+    },
+    // 执行echart方法showLoading
+    showLoading: function (type: string, options: any) {
+      this.delegateMethod('showLoading', type, options)
+    },
+    // 执行echart方法hideLoading
+    hideLoading: function () {
+      this.delegateMethod('hideLoading')
+    },
+    // 执行echart方法appendData
+    getDataURL: function (options: any) {
+      this.delegateMethod('getDataURL', options)
+    },
+    // 执行echart方法appendData
+    getConnectedDataURL: function (options: any) {
+      this.delegateMethod('getConnectedDataURL', options)
+    },
+    clear: function () {
+      this.delegateMethod('clear')
+    },
+    // 合并参数
+    mergeOptions: function (options: any, notMerge: boolean, lazyUpdate?: boolean) {
+      if (this.manualUpdate) {
+        this.manualOptions = options
+      }
+      if (!this.chart) {
+        this.init()
+      } else {
+        this.delegateMethod('setOption', options, notMerge, lazyUpdate)
+      }
     }
   },
   render () {
